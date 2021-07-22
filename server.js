7b7c2daba5ec9f62739feb58db64928aca5f6f0a
Khengsoon <Laukhengsoon@gmail.com>
2021-07-22 20:36:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(req,res) {
 	res.writeHead(200, {'Content-type': 'text/plain'});
-	res.end('Hello World.');
+	res.end('Hello World..');
 	
 }).listen(80)
 
